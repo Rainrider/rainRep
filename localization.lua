455c73be3fe92005af659c56358c64560d7f39d8
Rainrider <Rainrider@users.noreply.github.com>
2016-04-25 19:29:37 +0200

    Clean up the localization

--- localization.lua
+++ localization.lua
@@ -1,28 +1,12 @@
 ﻿local _, ns = ...
 
 local L = {
-	["more to"] = "more to",
-	["repetitions"] = "repetitions",
-	["Reputation changes in"] = "Reputation changes in",
-	["the end of"] = "the end of",
-	["the beginning of"] = "the beginning of",
-	["Database reset."] = "Database reset.",
-	["Stopped debugging."] = "Stopped debugging.",
-	["Started debugging."] = "Started debugging.",
 	["Unknown command:"] = "Unknown command:",
 	["No reputation changes."] = "No reputation changes.",
 }
-local locale = GetLocale()
+local locale = _G.GetLocale()
 
 if locale == "deDE" then
-	L["more to"] = "noch bis"
-	L["repetitions"] = "Wiederholungen"
-	L["Reputation changes in"] = "Rufänderung in"
-	L["the end of"] = "zum Anfang von"
-	L["the beginning of"] = "zum Ende von"
-	L["Database reset."] = "Datenbank zurückgesetzt."
-	L["Stopped debugging."] = "Debuggen angehalten."
-	L["Started debugging."] = "Debuggen gestartet."
 	L["Unknown command:"] = "Unbekannter Befehl:"
 	L["No reputation changes."] = "Keine Rufänderungen."
 end
